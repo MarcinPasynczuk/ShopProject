8d5016e7c00bb56c134ce683b05de4005ae3df84
MarcinPasynczuk <110817419+MarcinPasynczuk@users.noreply.github.com>
2022-08-09 10:37:44 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+let shopitemsdata = [{
+    id: "sddasfasf",
+    name: "Coleslaw",
+    price: 3,
+    desc: "lorem7",
+    img: "images/coleslaw.jpg"
+},
+{
+    id: "sddassdsfasf",
+    name: "Tzatziki",
+    price: 7,
+    desc: "lorbnjkhem7",
+    img: "images/tzatziki.jpg"
+},
+{
+    id: "sasfsdsasf",
+    name: "Olives",
+    price: 5,
+    desc: "loremhj7",
+    img: "images/olives.jpg"
+},
+{
+    id: "axczvzsfasf",
+    name: "Humus",
+    price: 4,
+    desc: "lorhjem7",
+    img: "images/humus.jpg"
+}
+];
